f1cbf8aaded3fa5297341cb3584f857e0aec9a80
Luiz <luizfernando.silva@outlook.com>
2022-06-29 17:13:13 -0300

    Creates exclusion routine for establishment and employee

--- src/main/java/br/edu/infnet/estabelecimentoapp/controller/EstabelecimentoController.java
+++ src/main/java/br/edu/infnet/estabelecimentoapp/controller/EstabelecimentoController.java
@@ -1,11 +1,15 @@
 package br.edu.infnet.estabelecimentoapp.controller;
 
+import br.edu.infnet.estabelecimentoapp.model.Estabelecimento;
 import br.edu.infnet.estabelecimentoapp.service.EstabelecimentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class EstabelecimentoController {
@@ -21,8 +25,20 @@ public class EstabelecimentoController {
 
     @GetMapping(value = "/funcionarios/{id}")
     public String telaListFuncionarios(Model model, @PathVariable Integer id){
+        Optional<Estabelecimento> estabelecimento = estabelecimentoService.listarEstabelecimentos()
+                .stream()
+                .filter(e -> e.getId().equals(id))
+                .findFirst();
+
         model.addAttribute("lista", estabelecimentoService.listarFuncionariosPorIdEstabelecimento(id));
+        estabelecimento.ifPresent(value -> model.addAttribute("estabelecimento", value.getNome()));
         return "funcionarios";
+    }
+
+    @GetMapping(value = "usuario/{id}")
+    public String excluirUsuario(@PathVariable Integer id){
+        estabelecimentoService.excluir(id);
+        return "redirect:/estabelecimentos";
     }
 
 }
